7ba8e5c2ec0665294d79857f5aa0a8bf399e10bb
Vezhys <133751736+vezhys@users.noreply.github.com>
2025-05-27 20:03:53 +0300

    Create DataSeeder.cs

---
+++ squares-api-exercise/Data/DataSeeder.cs
@@ -0,0 +1,27 @@
+﻿using squares_api_excercise.Models;
+using System;
+
+namespace squares_api_excercise.Data
+{
+    public class DataSeeder
+    {
+        public static async Task SeedAsync(SquaresDbContext context)
+        {
+            if (!context.Points.Any())
+            {
+                await context.Points.AddRangeAsync(new[]
+                {
+                    new Point { X = 0, Y = 0 },
+                    new Point { X = 0, Y = 1 },
+                    new Point { X = 1, Y = 0 },
+                    new Point { X = 1, Y = 1 },
+                    new Point { X = 2, Y = 2 },
+                    new Point { X = 2, Y = 3 },
+
+                });
+
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
